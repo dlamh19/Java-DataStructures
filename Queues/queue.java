2a17ea31ca3d4288c0bf076f822b86a706fdca50
David Lam <dlamh19@gmail.com>
2020-10-11 00:40:08 -0400

    init

---
+++ Queues/queue.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class queue 
+{
+    public static void main(String[] args)
+    {
+        Queue<Integer> queue = new queue()<>;
+
+        for(int i = 0; i <= 5; i++)
+            queue.add(i);
+
+    }
+}
